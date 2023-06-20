b30701d1176522a03b15aa90d161a35c088fc8ec
Dana Alami <dana.alami18@gmail.com>
2023-06-20 12:01:36 +0100

    fixted currenTime error

--- script.js
+++ script.js
@@ -12,26 +12,26 @@ const nextBtn = document.getElementById('next');
 
 // Music
 const songs = [
-    {
-        name: 'jacinto-1',
-        displayName: 'Electric Chill Machine',
-        artist: 'Jacinto Dessign',
-    },
-    {
-        name: 'jacinto-2',
-        displayName: 'Seven Nation Army (Remix)',
-        artist: 'Jacinto Dessign',
-    },
-    {
-        name: 'jacinto-3',
-        displayName: 'Goodnight, Disco Queen',
-        artist: 'Jacinto Dessign',
-    },
-    {
-        name: 'metric-1',
-        displayName: 'Front Row (Remix)',
-        artist: 'Jacinto Dessign',
-    },
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
 ];
 
 // Check if Playing
@@ -39,29 +39,29 @@ let isPlaying = false;
 
 // Play
 function playSong() {
-    isPlaying = true;
-    playBtn.classList.replace('fa-play', 'fa-pause');
-    playBtn.setAttribute('title', 'Pause');
-    music.play();
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
 }
 
-// Pause 
+// Pause
 function pauseSong() {
-    isPlaying = false;
-    playBtn.classList.replace('fa-pause', 'fa-play');
-    playBtn.setAttribute('title', 'Play');
-    music.pause();
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
 }
 
-// play or pause event listener
+// Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 
 // Update DOM
 function loadSong(song) {
-    title.textContent = song.displayName;
-    artist.textContent = song.artist;
-    music.src = `music/${song.name}.mp3`;
-    image.src = `img/${song.name}.jpg`;
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
 }
 
 // Current Song
@@ -69,61 +69,61 @@ let songIndex = 0;
 
 // Previous Song
 function prevSong() {
-    songIndex--;
-    if(songIndex < 0) {
-        songIndex = songs.length - 1;
-    }
-    loadSong(songs[songIndex]);
-    playSong();
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
 }
+
 // Next Song
 function nextSong() {
-    songIndex++;
-    if(songIndex > songs.length - 1) {
-        songIndex = 0;
-    }
-    loadSong(songs[songIndex]);
-    playSong();
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
 }
 
-// On Load - Select Fist Song
+// On Load - Select First Song
 loadSong(songs[songIndex]);
 
-// Update Progress Bar and Time
+// Update Progress Bar & Time
 function updateProgressBar(e) {
-    if (isPlaying) {
-      const { duration, currentTime } = e.srcElement;
-      // Update progress bar width
-      const progressPercent = (currentTime / duration) * 100;
-      progress.style.width = `${progressPercent}%`;
-      // Calculate display for duration
-      const durationMinutes = Math.floor(duration / 60);
-      let durationSeconds = Math.floor(duration % 60);
-      if (durationSeconds < 10) {
-        durationSeconds = `0${durationSeconds}`;
-      }
-      // Delay switching duration Element to avoid NaN
-      if (durationSeconds) {
-        durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
-      }
-      // Calculate display for currentTime
-      const currentMinutes = Math.floor(currentTime / 60);
-      let currentSeconds = Math.floor(currentTime % 60);
-      if (currentSeconds < 10) {
-        currentSeconds = `0${currentSeconds}`;
-      }
-      currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    // Update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    // Calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
     }
+    // Delay switching duration Element to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    // Calculate display for currentTime
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
-  
+}
+
 // Set Progress Bar
 function setProgressBar(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const { duration } = music;
-    music.currentTime = (clickX / width) * duration;
-  }  
-  
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTimeEl = (clickX / width) * duration;
+}
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
